8d523471517ac33a20b684b88f1d90384c2017ec
coralcat <sori.coralcat@gmail.com>
2020-08-03 16:48:25 +0900

    Updates

---
+++ precache-manifest.fe3ec85f9fc39029a0af89ece1ccd054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1e0dcc777829223d166268225749d1",
+    "url": "/coralcat/react-ghpages-test.git/index.html"
+  },
+  {
+    "revision": "0e9bdcfc7133b6bd2589",
+    "url": "/coralcat/react-ghpages-test.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9753123184264cfe6b87",
+    "url": "/coralcat/react-ghpages-test.git/static/js/2.a0e1a1bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coralcat/react-ghpages-test.git/static/js/2.a0e1a1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9bdcfc7133b6bd2589",
+    "url": "/coralcat/react-ghpages-test.git/static/js/main.89ef9643.chunk.js"
+  },
+  {
+    "revision": "510ed354afe199cc10f4",
+    "url": "/coralcat/react-ghpages-test.git/static/js/runtime-main.a400a0d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coralcat/react-ghpages-test.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
